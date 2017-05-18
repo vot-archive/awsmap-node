a7ef7806a6a0d88d66f9c4f0820dff42c5af5286
Vot Z <vot@siliconfen.co>
2017-05-18 20:49:30 +0100

    0.0.5 - usable programmatically

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-#!/usr/bin/env node
-return require('./cli');
+var awsmap = {
+  config: require('./lib/readConfig'),
+  mapEC2: require('./lib/mapEC2'),
+  mapELB: require('./lib/mapELB'),
+  mapS3: require('./lib/mapS3'),
+  printResultsEC2: require('./lib/printResultsEC2'),
+  printResultsELB: require('./lib/printResultsELB'),
+  printResultsS3: require('./lib/printResultsS3')
+}
+
+module.exports = awsmap;
